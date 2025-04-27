538a5ef20159eb453b075b9fb048b36180d428c0
Steven Loaiza <steven.loaiza@outlook.com>
2025-04-27 12:13:35 -0400

    Create output variable for lambda func name

--- learn-terraform-aws-lambda/outputs.tf
+++ learn-terraform-aws-lambda/outputs.tf
@@ -11,3 +11,10 @@ output "lambda_bucket_name" {
 
   value = aws_s3_bucket.lambda_bucket.id
 }
+
+// Output the value for the lambda function's name
+output "function_name" {
+  description = "Name of the Lambda function."
+
+  value = aws_lambda_function.hello_world.function_name
+}
